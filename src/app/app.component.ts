4f118c247dd03f03cd38c4430516045604ac4201
cavanosa <cavanosa@cavanosa.cav>
2023-12-08 01:06:53 +0100

    main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ResourceService } from './resource.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-gateway';
+  msguser = '';
+  msgadmin = '';
+
+
+  constructor(
+    private resourceService: ResourceService
+  ) {}
+
+  user(): void {
+    this.resourceService.user().subscribe({
+      next: (res: any) => (this.msguser = res.message),
+      error: (err: any) => (this.msguser = err.statusText + ': ' + err.status)
+    });
+  }
+
+  admin(): void {
+    this.resourceService.admin().subscribe({
+      next: (res: any) => (this.msgadmin = res.message),
+      error: (err: any) => (this.msgadmin = err.statusText + ': ' + err.status)
+    });
+  }
+
+  onLogin(): void {
+    window.location.href = '/oauth2/authorization/gateway';
+  }
 }
